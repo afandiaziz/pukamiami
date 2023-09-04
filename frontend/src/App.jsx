ac7a3e9c77a5f14309aec45bf5c102fb04df309f
FadilaPrasetyo8 <fadilaprasetyo8@gmail.com>
2023-09-04 14:27:09 +0700

    feat: Slicing Login Pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,11 +2,17 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import LoginPages from "./Pages/Login";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <></>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<LoginPages />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
