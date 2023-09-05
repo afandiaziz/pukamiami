d64150cc663a66c753347aa81329f78da2dfe93a
FadilaPrasetyo8 <fadilaprasetyo8@gmail.com>
2023-09-05 22:15:32 +0700

    feat: Slicing Register Pages

--- frontend/src/Pages/Register/index.jsx
+++ frontend/src/Pages/Register/index.jsx
@@ -1,11 +1,8 @@
 import React from "react";
 import Background from "../../assets/Background.svg";
 import logo from "../../assets/logo.svg";
-import Google from "../../assets/Google.svg";
-import { useNavigate } from "react-router-dom";
 
-export default function LoginPages() {
-  const navigate = useNavigate();
+export default function RegisterPages() {
   return (
     <>
       <div
@@ -16,7 +13,7 @@ export default function LoginPages() {
         }}
       >
         <div className="container mx-auto ">
-          <div className="grid grid-cols-1 lg:grid-cols-2 md:grid-cols-1 pt-40 place-items-center">
+          <div className="grid grid-cols-1 lg:grid-cols-2 md:grid-cols-1 pt-4 place-items-center">
             <div className="mx-auto hidden lg:block">
               <img className="w-3/4" src={logo} alt="" />
             </div>
@@ -25,7 +22,6 @@ export default function LoginPages() {
                 <div className="bg-[#FCAC5A] py-4 rounded-lg">
                   <h1 className=" text-white text-center text-xl">SIGN IN</h1>
                 </div>
-
                 <form className="px-8 pt-6 pb-8 mb-4">
                   <div className="mb-4">
                     <label
@@ -41,6 +37,31 @@ export default function LoginPages() {
                       placeholder="Username"
                     />
                   </div>
+                  <div className="mb-4">
+                    <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
+                      Email
+                    </label>
+                    <input
+                      className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                      id="email"
+                      type="email"
+                      placeholder="email"
+                    />
+                  </div>
+                  <div className="mb-4">
+                    <label
+                      className="block text-gray-700 text-sm font-bold mb-2"
+                      htmlFor="phone_number"
+                    >
+                      Phone Number
+                    </label>
+                    <input
+                      className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                      id="phone_number"
+                      type="number"
+                      placeholder="phone_number"
+                    />
+                  </div>
                   <div className="mb-6">
                     <label
                       className="block text-gray-700 text-sm font-bold mb-2"
@@ -51,6 +72,20 @@ export default function LoginPages() {
                     <input
                       className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
                       id="password"
+                      type="password"
+                      placeholder="******************"
+                    />
+                  </div>
+                  <div className="mb-6">
+                    <label
+                      className="block text-gray-700 text-sm font-bold mb-2"
+                      htmlFor="confirm_password"
+                    >
+                      Confirm Password
+                    </label>
+                    <input
+                      className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+                      id="confirm_password"
                       type="password"
                       placeholder="******************"
                     />
@@ -77,12 +112,6 @@ export default function LoginPages() {
                     Forgot Password?
                   </a>
                 </form>
-              </div>
-              <div className="text-center">
-                <p>Don’t have an account yet?</p>
-                <div className="text-blue-600 cursor-pointer" onClick={() => navigate(`/register`)}>
-                  Create an Account
-                </div>
               </div>
             </div>
           </div>
