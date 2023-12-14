276f861f65adfc73a1faa22b0cd9425976e3e630
afandiaziz <afandiaziz46@gmail.com>
2023-12-14 10:41:23 +0700

    refactor: users & auth controller

--- backend/app/Controllers/Http/AuthController.ts
+++ backend/app/Controllers/Http/AuthController.ts
@@ -4,114 +4,116 @@ import User from 'App/Models/User'
 
 export default class AuthController {
 
-   public async login({ auth, request, response }: HttpContextContract) {
-      await request.validate({
-         schema: schema.create({
-            email: schema.string({}, [
-               rules.email()
-            ]),
-            password: schema.string({}, [
-               rules.minLength(6)
-            ])
-         }),
-         messages: {
-            'email.required': 'Email tidak boleh kosong',
-            'email.email': 'Email tidak valid',
-            'password.required': 'Password tidak boleh kosong',
-            'password.minLength': 'Password minimal 6 karakter'
-         }
-      })
+    public async login({ auth, request, response }: HttpContextContract) {
+        await request.validate({
+            schema: schema.create({
+                email: schema.string({}, [
+                    rules.email()
+                ]),
+                password: schema.string({}, [
+                    rules.minLength(6)
+                ])
+            }),
+            messages: {
+                'email.required': 'Email tidak boleh kosong',
+                'email.email': 'Email tidak valid',
+                'password.required': 'Password tidak boleh kosong',
+                'password.minLength': 'Password minimal 6 karakter'
+            }
+        })
 
-      const { email, password } = request.all()
-      await auth.use('api').attempt(email, password, {
-         expiresIn: '30 mins'
-      }).then(async (result) => {
-         // result.user
-         await result.user.load('role', query => query)
-         // data.toJSON()
-         const data = result.user.toJSON()
-         data.token = result
-
-         return response.ok({
-            message: 'success',
-            data
-         })
-      }).catch(() => {
-         return response.unauthorized({
-            error: 'Invalid credentials'
-         })
-      })
-   }
-
-   public async logout({ auth, response }: HttpContextContract) {
-      await auth.use('api').authenticate()
-      await auth.use('api').revoke()
-      return response.ok({
-         message: 'success'
-      })
-   }
-
-   public async register({ auth, request, response }: HttpContextContract) {
-      const req = request.all()
-      await request.validate({
-         schema: schema.create({
-            name: schema.string(),
-            phone: schema.string({}, [
-               rules.mobile(),
-            ]),
-            email: schema.string({}, [
-               rules.email(),
-               rules.unique({ table: 'users', column: 'email' })
-            ]),
-            password: schema.string({}, [
-               rules.minLength(6),
-            ]),
-            confirm: schema.string({}, [
-               rules.minLength(6),
-               rules.confirmed('password')
-            ])
-         }),
-         messages: {
-            'name.required': 'Nama tidak boleh kosong',
-            'phone.required': 'Nomor HP tidak boleh kosong',
-            'phone.mobile': 'Nomor HP tidak valid',
-            'email.required': 'Email tidak boleh kosong',
-            'email.email': 'Email tidak valid',
-            'email.unique': 'Email sudah terdaftar',
-            'password.required': 'Password tidak boleh kosong',
-            'password.minLength': 'Password minimal 6 karakter',
-            'password.confirmed': 'Password tidak sama',
-            'confirm.minLength': 'Konfirmasi password minimal 6 karakter',
-            'confirm.required': 'Konfirmasi password tidak boleh kosong',
-         }
-      })
-
-      await User.create({
-         email: req.email,
-         password: req.password,
-         phone: req.phone,
-         name: req.name,
-      }).then(async (user) => {
-         await auth.use('api').attempt(req.email, req.password, {
+        const { email, password } = request.all()
+        await auth.use('api').attempt(email, password, {
             expiresIn: '30 mins'
-         }).then(async (token) => {
-            await user.load('role', query => query)
-            const data = user.toJSON()
-            data.token = token
-            return response.created({
-               message: 'success',
-               data,
-            })
-         }).catch(() => {
-            return response.unauthorized({
-               error: 'Invalid credentials'
-            })
-         })
-      }).catch(() => {
-         return response.badRequest({
-            error: 'Failed to create user'
-         })
-      })
+        }).then(async (result) => {
+            // result.user
+            await result.user.load('role', query => query)
+            // data.toJSON()
+            const data = result.user.toJSON()
+            data.token = result
 
-   }
+            return response.ok({
+                message: 'success',
+                data
+            })
+        }).catch(() => {
+            return response.notFound({
+                error: 'User Not Found'
+            })
+        })
+    }
+
+    public async logout({ auth, response }: HttpContextContract) {
+        await auth.use('api').authenticate()
+        await auth.use('api').revoke()
+        return response.ok({
+            message: 'success'
+        })
+    }
+
+    public async register({ auth, request, response }: HttpContextContract) {
+        const req = request.all()
+        await request.validate({
+            schema: schema.create({
+                name: schema.string(),
+                phone: schema.string({}, [
+                    rules.mobile(),
+                    rules.unique({ table: 'users', column: 'phone' })
+                ]),
+                email: schema.string({}, [
+                    rules.email(),
+                    rules.unique({ table: 'users', column: 'email' })
+                ]),
+                password: schema.string({}, [
+                    rules.minLength(6),
+                ]),
+                confirm: schema.string({}, [
+                    rules.minLength(6),
+                    rules.confirmed('password')
+                ])
+            }),
+            messages: {
+                'name.required': 'Nama tidak boleh kosong',
+                'phone.required': 'Nomor HP tidak boleh kosong',
+                'phone.mobile': 'Nomor HP tidak valid',
+                'phone.unique': 'Nomor HP sudah terdaftar',
+                'email.required': 'Email tidak boleh kosong',
+                'email.email': 'Email tidak valid',
+                'email.unique': 'Email sudah terdaftar',
+                'password.required': 'Password tidak boleh kosong',
+                'password.minLength': 'Password minimal 6 karakter',
+                'password.confirmed': 'Password tidak sama',
+                'confirm.minLength': 'Konfirmasi password minimal 6 karakter',
+                'confirm.required': 'Konfirmasi password tidak boleh kosong',
+            }
+        })
+
+        await User.create({
+            email: req.email,
+            password: req.password,
+            phone: req.phone,
+            name: req.name,
+        }).then(async (user) => {
+            await auth.use('api').attempt(req.email, req.password, {
+                expiresIn: '30 mins'
+            }).then(async (token) => {
+                await user.load('role', query => query)
+                const data = user.toJSON()
+                data.token = token
+                return response.created({
+                    message: 'success',
+                    data,
+                })
+            }).catch(() => {
+                return response.unauthorized({
+                    error: 'Invalid credentials'
+                })
+            })
+        }).catch(() => {
+            return response.badRequest({
+                error: 'Failed to create user'
+            })
+        })
+
+    }
 }
--- backend/app/Controllers/Http/UsersController.ts
+++ backend/app/Controllers/Http/UsersController.ts
@@ -4,73 +4,75 @@ import { schema, rules } from '@ioc:Adonis/Core/Validator'
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
 export default class UsersController {
-   public async profile({ auth, response }: HttpContextContract) {
-      const userId = await auth.use('api').authenticate().then((user) => user.id)
-      const user = await User.query().where('id', userId).preload('role').firstOrFail()
+    public async profile({ auth, response }: HttpContextContract) {
+        const userId = await auth.use('api').authenticate().then((user) => user.id)
+        const user = await User.query().where('id', userId).preload('role').firstOrFail()
 
-      response.ok({
-         message: 'success',
-         data: user
-      })
-   }
+        response.ok({
+            message: 'success',
+            data: user
+        })
+    }
 
-   public async updateProfile({ auth, request, response }: HttpContextContract) {
-      await request.validate({
-         schema: schema.create({
-            name: schema.string(),
-            phone: schema.string({}, [
-               rules.mobile()
-            ])
-         }),
-         messages: {
-            'name.required': 'Nama tidak boleh kosong',
-            'phone.required': 'Nomor HP tidak boleh kosong',
-            'phone.mobile': 'Nomor HP tidak valid'
-         }
-      })
+    public async updateProfile({ auth, request, response }: HttpContextContract) {
+        await request.validate({
+            schema: schema.create({
+                name: schema.string(),
+                phone: schema.string({}, [
+                    rules.mobile(),
+                    rules.unique({ table: 'users', column: 'phone', whereNot: { id: auth.use('api').user?.id } })
+                ])
+            }),
+            messages: {
+                'name.required': 'Nama tidak boleh kosong',
+                'phone.required': 'Nomor HP tidak boleh kosong',
+                'phone.mobile': 'Nomor HP tidak valid',
+                'phone.unique': 'Nomor HP sudah terdaftar'
+            }
+        })
 
-      const { name, phone } = request.all()
-      const user = await auth.use('api').authenticate()
+        const { name, phone } = request.all()
+        const user = await auth.use('api').authenticate()
 
-      user.name = name
-      user.phone = phone
-      await user.save()
+        user.name = name
+        user.phone = phone
+        await user.save()
 
-      const userUpdated = await User.query().where('id', user.id).preload('role').firstOrFail()
+        const userUpdated = await User.query().where('id', user.id).preload('role').firstOrFail()
 
-      response.ok({
-         message: 'success',
-         data: userUpdated
-      })
-   }
+        response.ok({
+            message: 'success',
+            data: userUpdated
+        })
+    }
 
-   public async updatePassword({ auth, request, response }: HttpContextContract) {
-      await request.validate({
-         schema: schema.create({
-            old_password: schema.string({}, [
-               rules.minLength(6)
-            ]),
-            new_password: schema.string({}, [
-               rules.minLength(6),
-            ])
-         }),
-         messages: {
-            'old_password.minLength': 'Password lama minimal 6 karakter',
-            'old_password.required': 'Password lama tidak boleh kosong',
-            'new_password.required': 'Password baru tidak boleh kosong',
-            'new_password.minLength': 'Password baru minimal 6 karakter',
-         }
-      })
+    public async updatePassword({ auth, request, response }: HttpContextContract) {
+        await request.validate({
+            schema: schema.create({
+                old_password: schema.string({}, [
+                    rules.minLength(6)
+                ]),
+                new_password: schema.string({}, [
+                    rules.minLength(6),
+                ])
+            }),
+            messages: {
+                'old_password.minLength': 'Password lama minimal 6 karakter',
+                'old_password.required': 'Password lama tidak boleh kosong',
+                'new_password.required': 'Password baru tidak boleh kosong',
+                'new_password.minLength': 'Password baru minimal 6 karakter',
+            }
+        })
 
-      const { old_password, new_password } = request.all()
-      const user = await auth.use('api').authenticate()
+        const { old_password, new_password } = request.all()
+        const user = await auth.use('api').authenticate()
 
-      await auth.use('api').attempt(user.email, old_password)
-      user.password = await Hash.make(new_password)
-      await user.save()
+        await auth.use('api').attempt(user.email, old_password)
+        user.password = await Hash.make(new_password)
+        await user.save()
 
-      response.ok({
-         message: 'success',
-      })
-   }
+        response.ok({
+            message: 'success',
+        })
+    }
 }
